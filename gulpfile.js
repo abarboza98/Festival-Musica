898373bb882dcd9657e8066a87893d18d6f2b447
Agustin Barboza <abarboza98@outlook.com>
2023-05-05 02:34:44 -0300

    Primer Commit

---
+++ gulpfile.js
@@ -0,0 +1,19 @@
+const { src, dest, watch } = require('gulp');
+const sass = require('gulp-sass')(require('sass'));
+
+function css(done) {
+  src('src/scss/app.scss') //Identificar el archivo de sass
+    .pipe(sass()) //COmpilar el archivo de SASS
+
+    .pipe(dest('build/css')); //Guardarlo en dico duro
+
+  done();
+}
+
+function dev(done) {
+  watch('src/scss/app.scss', css);
+
+  done();
+}
+exports.css = css;
+exports.dev = dev;
