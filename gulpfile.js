ff10fe7099f6b0f247ab158f7d1393448097b7d2
Agustin Barboza <abarboza98@outlook.com>
2023-05-07 01:18:30 -0300

    Header listo

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,10 @@
 const { src, dest, watch } = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
+const plumber = require('gulp-plumber');
 
 function css(done) {
   src('src/scss/**/*.scss') //Identificar el archivo de sass
+    .pipe(plumber())
     .pipe(sass()) //COmpilar el archivo de SASS
 
     .pipe(dest('build/css')); //Guardarlo en dico duro
