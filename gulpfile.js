4980f5cf3183d4aa562e9b4b31e0e8825f6a3dbe
Agustin Barboza <abarboza98@outlook.com>
2023-05-29 23:19:00 -0300

    New Feature: Trnasformando imagenes a formato webp

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,10 @@
-const { src, dest, watch } = require('gulp');
+const { src, dest, watch, parallel } = require('gulp');
+//CSS
 const sass = require('gulp-sass')(require('sass'));
 const plumber = require('gulp-plumber');
+
+//Imagenes
+const webp = require('gulp-webp');
 
 function css(done) {
   src('src/scss/**/*.scss') //Identificar el archivo de sass
@@ -12,10 +16,20 @@ function css(done) {
   done();
 }
 
+function versionWebp(done) {
+  const options = {
+    quality: 50,
+  };
+  src('src/img/**/*.{png, jpg}').pipe(webp(options)).pipe(dest('build/img'));
+
+  done();
+}
+
 function dev(done) {
   watch('src/scss/**/*.scss', css);
 
   done();
 }
 exports.css = css;
-exports.dev = dev;
+exports.versionWebp = versionWebp;
+exports.dev = parallel(versionWebp, dev);
