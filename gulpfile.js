8d14a21567efb3ae2d0e2a162db515cc96e27450
abarboza98 <abarboza98@outlook.com>
2023-06-27 16:21:09 -0300

    Convirtiendo imagenes en formato webp y formato avif

--- gulpfile.js
+++ gulpfile.js
@@ -4,7 +4,10 @@ const sass = require('gulp-sass')(require('sass'));
 const plumber = require('gulp-plumber');
 
 //Imagenes
+const cache = require('gulp-cache');
+const imageMin = require('gulp-imagemin');
 const webp = require('gulp-webp');
+const avif = require('gulp-avif');
 
 function css(done) {
   src('src/scss/**/*.scss') //Identificar el archivo de sass
@@ -12,6 +15,17 @@ function css(done) {
     .pipe(sass()) //COmpilar el archivo de SASS
 
     .pipe(dest('build/css')); //Guardarlo en dico duro
+
+  done();
+}
+
+function imagenes(done) {
+  const opciones = {
+    optimizationLevel: 3,
+  };
+  src('src/img/**/*.{png, jpg}')
+    .pipe(cache(imageMin(opciones)))
+    .pipe(dest('build/img'));
 
   done();
 }
@@ -25,6 +39,15 @@ function versionWebp(done) {
   done();
 }
 
+function versionAvif(done) {
+  const options = {
+    quality: 50,
+  };
+  src('src/img/**/*.{png, jpg}').pipe(avif(options)).pipe(dest('build/img'));
+
+  done();
+}
+
 function dev(done) {
   watch('src/scss/**/*.scss', css);
 
@@ -32,4 +55,6 @@ function dev(done) {
 }
 exports.css = css;
 exports.versionWebp = versionWebp;
-exports.dev = parallel(versionWebp, dev);
+exports.imagenes = imagenes;
+exports.versionAvif = versionAvif;
+exports.dev = parallel(imagenes, versionWebp, versionAvif, dev);
